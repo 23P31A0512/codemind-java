0923f5cf8c014accf94029a4d1bf5fa5655c75fb
23P31A0512 <147791827+23P31A0512@users.noreply.github.com>
2024-07-31 15:31:55 +0530

    import java.util.Scanner; public class mul{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int a,b;         a=read.nextInt();         b=read.nextInt();         int c=a*b;         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class mul{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int a,b;
+        a=read.nextInt();
+        b=read.nextInt();
+        int c=a*b;
+        System.out.println(c);
+    }
+}
